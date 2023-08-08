93f46cd7e9013f49658a3be789dd5057638a27b6
Matej Jazbinšek <jazbinsek@gmail.com>
2023-08-08 15:18:37 +0200

    button fix

--- src/lib/components/button/lButton.svelte
+++ src/lib/components/button/lButton.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-    export let type: 'button' | 'submit' | 'reset' = 'button';
-    export let onClick = () => {}; 
-    export let style: 'primary' | 'secondary' | 'testing' = 'primary';
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let onClick = () => {};
+	export let style: 'primary' | 'secondary' | 'testing' = 'primary';
 
-    let styleClasses = {
-    primary: 'btn btn-lg variant-filled-primary',
-    secondary: 'btn btn-sm variant-filled-secondary',
-    testing: 'btn btn-lg variant-filled-error',
-    };
+	let styleClasses = {
+		primary: 'btn btn-lg variant-filled-primary',
+		secondary: 'btn btn-sm variant-filled-secondary',
+		testing: 'btn btn-lg variant-filled-error'
+	};
 
-    console.log(styleClasses[style]);
-    $: customClass = styleClasses[style];
+	console.log(styleClasses[style]);
+	let customClass = styleClasses[style];
 </script>
-<button type="{type}" on:click="{onClick}" class="{customClass}"> <slot/></button>
+
+<button {type} on:click={onClick} class={customClass}> <slot /></button>
