983e8b57ac77fa34f3a591971860478be7ebb174
M1kk4 <sven.korosec@gmail.com>
2023-08-08 19:46:32 +0200

    add button style

--- src/lib/components/button/lButton.svelte
+++ src/lib/components/button/lButton.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	export let type: 'button' | 'submit' | 'reset' = 'button';
 	export let onClick = () => {};
-	export let style: 'primary' | 'secondary' | 'error_outline' = 'primary';
+	export let style: 'primary' | 'secondary' | 'error' | 'warning' | 'success' | 'lighter' | 'icon' = 'primary';
 
 	let styleClasses = {
 		primary: 'btn btn-lg variant-filled-primary',
 		secondary: 'btn btn-sm variant-filled-secondary',
-		error_outline: 'btn btn-lg variant-ringed-error',
+		error: 'btn btn-lg variant-filled-error',
+		warning: 'btn btn-lg variant-filled-warning',
+		success: 'btn btn-lg variant-filled-success',
+		lighter: 'btn btn-lg variant-soft-surface',
+		icon: 'btn-icon btn-icon-xl variant-filled'
 	};
 
 	console.log(styleClasses[style]);
@@ -15,5 +19,13 @@
 
 <button type={type} on:click={onClick} class={customClass}> <slot /></button>
 
+<button class="btn text-"></button>
 
 <!-- submit gumb, primary gumb, normaln gumb (malo svetlejše kot ozadje), outline gumb (warning, success, error -- rdec - error, zelen, rumen) -->
+
+
+<style lang="postcss">
+	.variant-filled-error {
+	@apply font-bold;
+	}
+</style>
