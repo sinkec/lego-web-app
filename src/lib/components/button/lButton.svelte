247d1cf8eafeb9d70c2b1385e2804b807198f319
M1kk4 <sven.korosec@gmail.com>
2023-08-08 15:08:19 +0200

    button component
    button component

---
+++ src/lib/components/button/lButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let type: 'button' | 'submit' | 'reset' = 'button';
+    export let onClick = () => {}; 
+    export let style: 'primary' | 'secondary' | 'testing' = 'primary';
+
+    let styleClasses = {
+    primary: 'btn btn-lg variant-filled-primary',
+    secondary: 'btn btn-sm variant-filled-secondary',
+    testing: 'btn btn-lg variant-filled-error',
+    };
+
+    console.log(styleClasses[style]);
+    $: customClass = styleClasses[style];
+</script>
+<button type="{type}" on:click="{onClick}" class="{customClass}"> <slot/></button>
---
+++ src/routes/components/button/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import LButton from "$lib/components/button/lButton.svelte";
+
+</script>
+<h2 class="h2">Button</h2>
+<LButton onClick={() => (console.log('Button clicked!'))} style='testing'>Hello</LButton>
