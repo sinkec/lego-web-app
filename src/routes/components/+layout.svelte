0dd18e5e9e7922409cbf5393809f573bc7fca8d3
Matej Jazbinšek <jazbinsek@gmail.com>
2023-08-08 10:44:09 +0200

    Creating - Component library
    List of all component with tailwind styling

---
+++ src/routes/components/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-64 p-5">
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Left side of NavBar -->
+		<nav class="list-nav">
+			<ul>
+				<li>
+					<a href="/elements/lists"> Input </a>
+				</li>
+				<li>
+					<a href="/elements/lists"> Button </a>
+				</li>
+				<li>
+					<a href="/elements/lists"> Login </a>
+				</li>
+				<li>
+					<a href="/elements/lists"> O nas </a>
+				</li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+</AppShell>
