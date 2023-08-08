0b58e00b06c85ec2c2f7be8d0035ce5f968b55c4
Matej Jazbinšek <jazbinsek@gmail.com>
2023-08-08 11:24:22 +0200

    Page layout for component library

--- src/routes/components/+layout.svelte
+++ src/routes/components/+layout.svelte
@@ -2,10 +2,15 @@
 	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-64 p-5">
+<AppShell slotSidebarLeft="bg-surface-500/5 w-64 p-5 shadow-md">
 	<svelte:fragment slot="sidebarLeft">
 		<!-- Left side of NavBar -->
 		<nav class="list-nav">
+			<div class="mb-5">
+				<h3 class="h3">Components</h3>
+				<p class="text-xs">List of components used in project</p>
+			</div>
+
 			<ul>
 				<li>
 					<a href="/elements/lists"> Input </a>
@@ -21,5 +26,8 @@
 				</li>
 			</ul>
 		</nav>
+	</svelte:fragment>
+	<svelte:fragment slot="default">
+		<div class="p-5"><slot /></div>
 	</svelte:fragment>
 </AppShell>
--- src/routes/components/+page.svelte
+++ src/routes/components/+page.svelte
@@ -1,2 +1,4 @@
 <script lang="ts">
 </script>
+
+<h2 class="h2">Components</h2>
